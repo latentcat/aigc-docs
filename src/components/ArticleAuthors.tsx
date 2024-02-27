1678d9407ea4707df5c62524f7a297c2cc12602f
ciaochaos <1272777550@qq.com>
2024-02-27 11:03:23 +0800

    update

---
+++ src/components/ArticleAuthors.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export function AuthorCiaochaos() {
+  return <Link href="https://troyni.com" target="_blank">Troy Ni</Link>
+}
+
+export function AuthorShichen() {
+  return <Link href="https://github.com/chenbaiyujason" target="_blank">Shichen</Link>
+}
+
+export function AuthorCpunisher() {
+  return <Link href="https://github.com/CPunisher" target="_blank">CPunisher</Link>
+}
