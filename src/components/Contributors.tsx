a89163c9626dfb26a9268f669d2d923bec83c8e8
倪豪 <1272777550@qq.com>
2024-02-29 02:02:11 +0800

    update

---
+++ src/components/Contributors.tsx
@@ -0,0 +1,37 @@
+
+
+import { promises as fs } from 'fs';
+
+interface Contributor {
+  login: string
+  name: string
+  avatar_url: string
+  profile: string
+  contributions: string[]
+}
+
+export default async function Contributors() {
+  const file = await fs.readFile(process.cwd() + '/.all-contributorsrc', 'utf8');
+  const data = JSON.parse(file);
+  const contributors: Contributor[] = data["contributors"]
+
+  return (
+    <div className="flex flex-wrap gap-6 ">
+      {contributors.map((contributor) => (
+        <div key={contributor.login} className="flex flex-col items-center _grow">
+          <a href={contributor.profile} className="flex flex-col items-center grow-0 w-20">
+            <img
+              src={contributor.avatar_url}
+              width="w-20 h-20"
+              alt={contributor.name}
+              className="rounded-full"
+            />
+            <div className="mt-2 text-xs line-clamp-1 w-full text-center">
+              <b>{contributor.name}</b>
+            </div>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
